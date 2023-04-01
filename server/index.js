eac35fb17b4c6699c71590be6ed8392058d4232b
AvishkarMandlik <mandlikavi121@gmail.com>
2023-04-01 22:05:59 +0530

    server connected to mongodb success

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(`${process.env.MONGO_URL}`, {useNewUrlParser: true,
+     useUnifiedTopology: true}, ()=>{
+    console.log("mongodb is connected...")
+});
