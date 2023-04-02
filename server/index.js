14b0164d3e34b2f5d07cf1b753cca7c7bb318c28
AvishkarMandlik <mandlikavi121@gmail.com>
2023-04-02 09:56:45 +0530

    signup api added

--- server/index.js
+++ server/index.js
@@ -4,12 +4,84 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
+import User from './models/User.js';
+
+
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(`${process.env.MONGO_URL}`, {useNewUrlParser: true,
-     useUnifiedTopology: true}, ()=>{
-    console.log("mongodb is connected...")
-});
+mongoose.connect(process.env.MONGODB_URL, ()=>{
+    console.log('Connected to MongoDB');
+})
+
+
+// api routes starts here
+// signup api starts here
+app.post('/signup', async (req, res) => {
+    const { name, phone, email, password, role } = req.body;
+
+// validation to check if all fields are filled starts here
+    const emptyFields = [];
+
+    if (!name) emptyFields.push('name');
+    if (!phone) emptyFields.push('phone');
+    if (!email) emptyFields.push('email');
+    if (!password) emptyFields.push('password');
+    if (!role) emptyFields.push('role');
+
+    if (emptyFields.length > 0) {
+        return res.json({
+            success: false,
+            message: `${emptyFields.join(', ')} are required`
+        })
+    }
+
+// validation to check if all fields are filled ends here
+
+
+// validation to check if email already exists starts here
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+        return res.json({
+            success: false,
+            message: "Email already exists"
+        })
+    }
+// validation to check if email already exists ends here
+
+// validation to check if phone already exists starts here
+    const existingUserPhone = await User.findOne({ phone: phone });
+    if (existingUserPhone) {
+        return res.json({
+            success: false,
+            message: "Phone already exists"
+        })
+    }
+// validation to check if phone already exists ends here
+
+    const user = new User({
+        name: name,
+        phone: phone,
+        email: email,
+        password: password,
+        role: role
+    })
+
+    const savedUser = await user.save();
+
+    res.json({
+        success: true,
+        message: "User created successfully",
+        data: savedUser
+    })
+})
+// signup api ends here
+
+
+// api routes ends here
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
