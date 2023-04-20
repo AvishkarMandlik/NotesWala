981110172e9ec2e389820f8b63fadc25795a96d7
AvishkarMandlik <mandlikavi121@gmail.com>
2023-04-20 23:08:30 +0530

    admin page route set

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Home from "./views/Home/Home"
-import Login from './views/Login/Login'
-import Signup from './views/Signup/Signup'
+import Home from "./views/Home/Home";
+import Login from './views/Login/Login';
+import Signup from './views/Signup/Signup';
+import Admin from './Admin/Admin';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login/>} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/Admin" element={<Admin />} />
         </Routes>
       </BrowserRouter>
     </div>
