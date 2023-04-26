2d833bb4c5976f59c181127eefb14e048ddc1880
AvishkarMandlik <mandlikavi121@gmail.com>
2023-04-26 19:22:02 +0530

    component folder added and pdf card file added

---
+++ client/src/components/PdfCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './PdfCard.css';
+import {Link} from 'react-router-dom';
+
+
+function PdfCard({year,description,imgUrl,faculty,title,pdfUrl}) {
+
+
+  return (
+    <>
+    <div className='col-md-3'>
+        <div className='pdf-item-card text-center'>
+            <div>
+            <img src={imgUrl} alt='pdf-png' className='pdf-item-card-header-img'/>
+            </div>
+            <h4>{title}</h4>
+            <p>{description}</p>
+            <p>{year}</p>
+            <span>{faculty}</span>
+            <Link to={pdfUrl}> <button className='btn-view-pdf'> View Pdf</button></Link> <br/>
+        </div>
+    </div>
+
+    </>
+  )
+}
+
+export default PdfCard
