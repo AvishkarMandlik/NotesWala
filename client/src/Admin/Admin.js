0d445948bad85c4086698f1128b80b445e8e70c3
AvishkarMandlik <mandlikavi121@gmail.com>
2023-05-04 06:33:23 +0530

    Admin page done

--- client/src/Admin/Admin.js
+++ client/src/Admin/Admin.js
@@ -1,54 +1,32 @@
-import React, { useState } from "react";
+import { currentUser } from "../util/currentUser";
 
-
-import "./Admin.css";
+import AddTypdf from "./AddTypdf/AddTypdf";
+import AddSypdf from "./AddSypdf/AddSypdf";
+import AddFypdf from "./AddFypdf/AddFypdf";
 
 function Admin() {
-
-
-  // Base64 conversion code starts here
-  const [baseImage, setBaseImage] = useState("");
-  const uploadImage = async (e) => {
-    const file = e.target.files[0];
-    const base64 = await convertBase64(file);
-    setBaseImage(base64);
-  };
-
-  const convertBase64 = (file) => {
-    return new Promise((resolve, reject) => {
-      localStorage.setItem("baseImage",setBaseImage)
-      const fileReader = new FileReader();
-      fileReader.readAsDataURL(file);
-
-      fileReader.onload = () => {
-        resolve(fileReader.result);
-      };
-
-      fileReader.onerror = (error) => {
-        reject(error);
-      };
-     
-    });
-
-    // Base64 conversion code end here
-  };
-
-  return (
-    <>
-     
-      <div className="Admin">
-        <input
-          type="file"
-          onChange={(e) => {
-            uploadImage(e);
-          }}
-        />
-        <br></br>
-        <img alt={baseImage} height="200px" />
+  if (currentUser.role === "admin") {
+    return (
+      <div class="container">
+        <h1 className="text-center">Welcome, {currentUser?.name} !</h1>
+        <div class="row">
+          <div class="col-md-4 mb-4">
+            <div class="card-body bg-dark text-white">{<AddFypdf />}</div>
+          </div>
+          <div class="col-md-4 mb-4">
+            <div class="card-body bg-dark text-white">{<AddSypdf />} </div>
+          </div>
+          <div class="col-md-4 mb-4">
+            <div class="card-body bg-dark text-white">{<AddTypdf />}</div>
+          </div>
+        </div>
       </div>
-   
-    </>
-  );
+    );
+  } 
+  else {
+    alert("You are not authorized to view this page");
+    window.location.href = "/";
+  }
 }
 
 export default Admin;
