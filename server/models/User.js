e97f2d59284ab3722b2cc81af1e5c299b8ccf629
AvishkarMandlik <mandlikavi121@gmail.com>
2023-07-14 11:49:32 +0530

    password encryption add but not compare a password on login

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 import {Schema, model} from "mongoose";
-
+// import bcrypt from "bcrypt";
 const userSchema = new Schema({
   name: String,
   email: String,
@@ -7,6 +7,29 @@ const userSchema = new Schema({
   password: String,
   role: String,
 }, { timestamps: true })
+
+// userSchema.pre("save", function (next) {
+//   const user = this
+
+//   if (this.isModified("password") || this.isNew) {
+//     bcrypt.genSalt(10, function (saltError, salt) {
+//       if (saltError) {
+//         return next(saltError)
+//       } else {
+//         bcrypt.hash(user.password, salt, function(hashError, hash) {
+//           if (hashError) {
+//             return next(hashError)
+//           }
+
+//           user.password = hash
+//           next()
+//         })
+//       }
+//     })
+//   } else {
+//     return next()
+//   }
+// })
 
 const User = model("User", userSchema)
 
