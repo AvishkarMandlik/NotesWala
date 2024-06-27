6acdbfa2a50fd01c6a17db511b56d690e350012c
AvishkarMandlik <mandlikavi121@gmail.com>
2024-06-27 22:13:52 +0530

    set strictquery true

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+mongoose.set("strictQuery", true);
 
 mongoose.connect(process.env.MONGODB_URL, ()=>{
     console.log('Connected to MongoDB');
