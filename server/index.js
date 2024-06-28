85520189ec96b63d32014ab5fc51a8c3ba7d2975
AvishkarMandlik <mandlikavi121@gmail.com>
2024-06-28 22:26:25 +0530

    mongoose connection old

--- server/index.js
+++ server/index.js
@@ -18,13 +18,10 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 mongoose.set("strictQuery", true);
 
-mongoose.connect(process.env.MONGODB_URL)
- .then(() => {
+mongoose.connect(process.env.MONGODB_URL, ()=>{
     console.log('Connected to MongoDB');
-  })
- .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+})
+
 
 // api routes starts here
 // signup api starts here
